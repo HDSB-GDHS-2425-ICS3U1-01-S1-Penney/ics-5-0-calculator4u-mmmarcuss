86e1b1f710e2af889ac4c619144bdde1aca0bc4d
mmmarcuss <1melomar@hdsb.ca>
2024-09-12 12:46:00 -0400

    Update index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+
+
+let num1;
+let num2;
+let expression;
+let answer;
+
+
+num1 = prompt("Welcome to calculator. I will ask you two numbers and an arithmitic expression. What should the first number be?");
+expression = prompt("what is our expression? Please enter it as a word.");
+num2 = prompt("what should our last number be?");
+
+
+if (expression = "addition") {
+    console.log(+num1 + +num2); 
+} 
+
+if (expression = "subtraction" ) {
+    console.log(+num1 - +num2);
+}
+
+if (expression = "multiplication" ) {
+    console.log(+num1 * +num2);
+}
+
+if (expression = "division" ) {
+    console.log(+num1 / +num2);
+}
+
