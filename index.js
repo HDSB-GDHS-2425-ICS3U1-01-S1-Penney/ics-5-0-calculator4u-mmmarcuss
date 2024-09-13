87f1b016a6fa819e3ed03af9ae085c7c51af15a5
mmmarcuss <1melomar@hdsb.ca>
2024-09-13 17:38:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
 
-
+let x = prompt("Welcome to calculator. I will ask you two numbers and an arithmitic expression. What should the first number be?", "0");
+let expression = prompt("what is our expression? Please enter it as a symbol. You may also enter 'square number' to square root, or '^' to raise the first number to the power of the second.");
 let num1;
 let num2;
-let expression;
-let answer;
 
+ function setToNumber () {
+    num1 = Number(x);
+    num2 = Number(y);
+ }
+ 
+ setToNumber ();
 
-num1 = prompt("Welcome to calculator. I will ask you two numbers and an arithmitic expression. What should the first number be?");
-expression = prompt("what is our expression? Please enter it as a word.");
-num2 = prompt("what should our last number be?");
+if (expression == "square number") {
+    alert(Math.sqrt(num1))
 
-
-if (expression = "addition") {
-    console.log(+num1 + +num2); 
-} 
-
-if (expression = "subtraction" ) {
-    console.log(+num1 - +num2);
+} else {
+    var y = prompt("what should our last number be?", "0");
+    setToNumber ();
+    
+    if (expression == "^") {
+        alert(Math.pow(num1, num2))
+    
+    } else if (expression == "+") {
+        alert(num1 + num2);
+    
+    } else if (expression == "-" ) {
+        alert(num1 - num2);
+    
+    } else if (expression == "*" ) {
+        alert(num1 * num2);
+    
+    } else if (expression == "/" ) {
+        alert(num1 / num2);
+    
+    } else {
+        alert("invaled entry. Please enter two numerical values, and an expression (+, -, *, or /). You may also square the first number, or raise the first number to the power of the second number.");
+    
+    }
 }
 
-if (expression = "multiplication" ) {
-    console.log(+num1 * +num2);
-}
-
-if (expression = "division" ) {
-    console.log(+num1 / +num2);
-}
 
